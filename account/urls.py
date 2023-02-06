8d21829b1ba8201232326552c744a55cd36fd40a
Vanshul Choudhary <cvanshul09@gmail.com>
2023-02-07 00:08:04 +0530

    .

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name = 'register'),
     path('login/', UserLoginView.as_view(), name = 'login'),
     path('profile/', UserProfileView.as_view(), name = 'profile'),
-    # path('logout/', UserLogoutView.as_view(), name = 'logout'),
     path('changepassword/', UserChangePasswordView.as_view(), name = 'changepassword'),
     path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
     path('password-reset-confirm/<uid>/<token>/', UserPasswordResetView.as_view(), name='password-reset-confirm')
